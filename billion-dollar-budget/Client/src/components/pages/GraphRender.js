a91914f019dd285016867e8722ef57c7b81099f4
robertarthury <robertarthury@gmail.com>
2022-07-06 18:57:15 -0400

    home page startup

---
+++ billion-dollar-budget/Client/src/components/pages/GraphRender.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GraphPic } from '../assets/images/graph.png'
+
+const GraphRender = () => {
+  return (
+    <div>
+      <img src={GraphPic} alt="gaph"/>
+    </div>
+  )
+}
+
+export default GraphRender
