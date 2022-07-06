e19f9f788d293b054403b59abb421661db377da1
chris chubb <chubb91chris@icloud.com>
2022-07-06 18:45:50 -0400

    added mongodb to server with models for user liabilities and assets

---
+++ billion-dollar-budget/server/models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User')
+const Assests = require('./Assests');
+const Liabilities = require('./Liabilities')
+
+
+module.exports ={User}
+module.exports = {Assests}
+module.exports = {Liabilities}
