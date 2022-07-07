18e4f177f924603ce45a11a41275ae48b9d188d3
chris chubb <chubb91chris@icloud.com>
2022-07-07 18:25:49 -0400

    start of apollo server

--- billion-dollar-budget/server/server.js
+++ billion-dollar-budget/server/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const {apolloServer} = require('apollo-server-express');
+
+const{typeDefs,resolvers} = require('./schemas');
 const db = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT|| 3001;
 
+const PORT = process.env.PORT|| 3001;
+const server = new apolloServer({
+    typeDefs,
+    resolvers
+});
+const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+const startApolloServer =async (typeDefs,resolvers)=>{
+    await server.start();
+server.applyMiddleware({app});
 db.once('open',()=>{
     app.listen(PORT,()=>{
         console.log(`server is now on ${PORT}!`)
     });
 })
+}
+
+startApolloServer(typeDefs,resolvers);
