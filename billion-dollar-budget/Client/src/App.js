b656a36ecfab639a9a4815ead22d0489be48622c
colinc27 <colinchristoph27@gmail.com>
2022-07-07 16:33:23 -0700

    Fixed top Styling for Header

--- billion-dollar-budget/Client/src/App.js
+++ billion-dollar-budget/Client/src/App.js
@@ -4,16 +4,15 @@ import Balance  from './components/pages/Balance';
 import IncomeExpenses from './components/pages/IncomeExpenses';
 import Chart from "./components/pages/Chart";
 import Footer from './components/pages/Footer';
-
 import './App.css';
 
 function App() {
   
   return (
       <body class="d-flex flex-column min-vh-100">
-        <div class="container">
-          <Header />
-          <div class="">
+        <div class="container pt-5">
+        <Header />
+          <div class="py-5">
             <Balance />
             <IncomeExpenses />
             <Chart />
--- billion-dollar-budget/Client/src/components/pages/Header.js
+++ billion-dollar-budget/Client/src/components/pages/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header class="fixed-top bg-success bg-gradient text-center text-white py-3">
-      <h2 class="">
-      Billion Dollar Budget App
-      </h2>
+    <header class="fixed-top bg-success bg-gradient text-center text-white py-1">
+      <h1 class="">
+      B$B
+      </h1>
     </header>
 
   )
