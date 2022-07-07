e70d116ec216ff2fe68d2f3fdc14fc94068e071f
chris chubb <chubb91chris@icloud.com>
2022-07-07 17:07:38 -0400

    add heroku

--- billion-dollar-budget/server/server.js
+++ billion-dollar-budget/server/server.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-
-
+app.listen(3001,()=>{
+    console.log('Server is on port 3001')
+})
 mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost:27017/billion-dollar-budget',{
     useNewUrlParser: true,
     useUnifiedTopology:true
