d9336fff08d3ca8619689e3b172be5520bcddbb4
robertarthury <robertarthury@gmail.com>
2022-07-07 17:43:35 -0400

    refractoring the income variables table to a responsive table

--- billion-dollar-budget/Client/src/components/pages/IncomeExpenses.js
+++ billion-dollar-budget/Client/src/components/pages/IncomeExpenses.js
@@ -1,77 +1,140 @@
-import React from 'react'
-import Table from 'react-bootstrap/Table'
+import React, { useState } from 'react'
 
-const IncomeExpenses = () => {
+
+
+function IncomeExpenses() {
+
+
+      const [inEditMode, setInEditMode] = useState({
+      status: false,
+      rowKey: null
+  });
+
+  const [ incomeSourceAmount, setIncomeSourceAmount] = useState(null);
+
+  const onEdit = ({id, incomeSourceAmount}) => {
+      setInEditMode({
+          status: true,
+          rowKey: id
+      })
+      setIncomeSourceAmount(incomeSourceAmount);
+}
+
+//fetch go here?
+
+const onSave = ({id, newIncomeSourceAmount}) => {
+  updateInventory({id, newIncomeSourceAmount});
+}
+
+const onCancel = () => {
+  // reset the inEditMode state value
+  setInEditMode({
+      status: false,
+      rowKey: null
+  })
+  // reset the unit price state value
+  setIncomeSourceAmount(null);
+}
+  
   return (
     <>
-        <h3>Monthly Income (fill in amounts to add your total income)</h3>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Income Source 1</td>
-            <td>$3800</td>
-          </tr>
-          <tr>
-            <td>Income Source 2</td>
-            <td>$2800</td>
-
-          </tr>
-          <tr>
-            <td>other</td>
-            <td>$1000</td>
-
-          </tr>
-        </tbody>
-      </Table>
-        <h3>Monthly Expense (fill in amounts to add your total expenses)</h3>
-        <Table striped bordered hover>
-          <thead>
+        <h3>Monthly Income</h3>
+      <div>
+        <table>
+            <thead>
             <tr>
-              <th>Item</th>
+              <th>Income Source</th>
               <th>Amount</th>
-            </tr>
-          </thead>
+            </tr> 
+            </thead>
           <tbody>
-            <tr>
-              <td>Utilities</td>
-              <td>$120</td>
-            </tr>
-            <tr>
-              <td>Cell Phone</td>
-              <td>$180</td>
-            </tr>
-            <tr>
-              <td>Groceries</td>
-              <td>$400</td>
-            </tr>
-            <tr>
-              <td>Total Car Payments</td>
-              <td>$400</td>
-            </tr>
-            <tr>
-              <td>Credit Card Payments</td>
-              <td>$400</td>
-            </tr>
+          {
+              data.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.income_source}</td>
+                  <td>
+                      {
+                          inEditMode.status && inEditMode.rowKey === user.id ? (
+                              <input value={incomeSourceAmount}
+                                 onChange={(event) => setIncomeSourceAmount(event.target.value)}
+                              />
+                          ) : (  
+                              user.income_source_amount
+                          )
+                      }
+                  </td>
+                  <td>
+                     {
+                        inEditMode.status && inEditMode.rowKey === item.id ? (
+                          <>
+                              <button                     
+                                className={"btn btn-success"}
+                                onClick={() => onSave({id: item.id, newUnitPrice: unitPrice})}
+                              >Save
+                              </button>
+
+                              <button
+                                className={"btn btn-secondary"}
+                                style={{marginLeft: 8}}
+                                onClick={() => onCancel()}
+                              >Cancel
+                              </button>
+                          </>
+                      ) : (
+                              <button
+                                className={"btn btn-primary"}
+                                onClick={() => onEdit({id: item.id, currentUnitPrice: item.unit_price})}
+                              >Edit</button>
+                                    )
+                      }
+                  </td>
+                </tr>
+              ))
+          }
           </tbody>
-        </Table>
-
-
+         </table>
+      </div>
+      <div>
+          <h3>Monthly Expense (fill in amounts to add your total expenses)</h3>
+          <Table>
+           <thead>
+              <tr>
+                <th>Item</th>
+                <th>Amount</th>
+             </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Utilities</td>
+                <td>$120</td>
+              </tr>
+             <tr>
+                <td>Cell Phone</td>
+                <td>$180</td>
+              </tr>
+              <tr>
+                <td>Groceries</td>
+                <td>$400</td>
+              </tr>
+              <tr>
+                <td>Total Car Payments</td>
+                <td>$400</td>
+              </tr>
+              <tr>
+                <td>Credit Card Payments</td>
+                <td>$400</td>
+              </tr>
+            </tbody>
+         </Table>
+         <hr/>
+        <input type='text' />
+        <button className='btn btn-primary'>add expenses</button>
+        </div>
       </>
 
-        )
-        }
+    );
+  }
 
 
-
-
-
-
-
-        export default IncomeExpenses
-  
+    export default IncomeExpenses;
+    
