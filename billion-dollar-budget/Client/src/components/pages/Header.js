67bfc1b78203b52166487b51fed3b958330d8874
colinc27 <colinchristoph27@gmail.com>
2022-07-07 17:34:29 -0700

    Changes to sidebar

--- billion-dollar-budget/Client/src/components/pages/Header.js
+++ billion-dollar-budget/Client/src/components/pages/Header.js
@@ -4,34 +4,32 @@ import { slide as Menu } from 'react-burger-menu';
 const Header = () => {
   /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
   return (
-    <header class="fixed-top bg-success bg-gradient text-center text-white py-1 flex-row">
+    <header class="fixed-top bg-success bg-gradient text-center text-white py-1">
       <h1 class="">
       B$B
       </h1>
       <Menu>
-      <a className="menu-item" href="/">
-        Home
-      </a>
+        <div>
+          <h5>Please enter your budget variables here</h5>
+          <div class="d-flex flex-row my-2">         
+          <h6 class="px-2">Monthly Budget</h6>
+             <input class="form-control form-control-sm " type="text" placeholder="$300"></input>
+             </div>
+             <div class="d-flex flex-row my-2">         
+          <h6 class="px-2">Expected Gas</h6>
+             <input class="form-control form-control-sm" type="text" placeholder="$300"></input>
+             </div>
+             <div class="d-flex flex-row my-2">         
+          <h6 class="px-2">Expected Rent</h6>
+             <input class="form-control form-control-sm" type="text" placeholder="$300"></input>
+             </div>
+             <div class="d-flex flex-row my-2">         
+          <h6 class="px-2">Expected Food</h6>
+             <input class="form-control form-control-sm" type="text" placeholder="$300"></input>
+             </div>    
+             <button type="button" class="btn btn-success">Save</button>
+        </div>
 
-      <a className="menu-item" href="/laravel">
-        Laravel
-      </a>
-
-      <a className="menu-item" href="/angular">
-        Angular
-      </a>
-
-      <a className="menu-item" href="/react">
-        React
-      </a>
-
-      <a className="menu-item" href="/vue">
-        Vue
-      </a>
-
-      <a className="menu-item" href="/node">
-        Node
-      </a>
     </Menu>
     </header>
 
