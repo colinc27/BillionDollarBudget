5eaba6e01e59a1d72f8e114ca06eeb7624ff6bd6
robertarthury <robertarthury@gmail.com>
2022-07-07 22:23:33 -0400

    working on these main forms

--- billion-dollar-budget/Client/src/components/pages/IncomeExpenses.js
+++ billion-dollar-budget/Client/src/components/pages/IncomeExpenses.js
@@ -1,29 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Table from 'react-bootstrap/Table'
 
 
 
 function IncomeExpenses() {
-
+      const [data, setData] = useState([]);
 
       const [inEditMode, setInEditMode] = useState({
       status: false,
       rowKey: null
   });
 
-  const [ incomeSourceAmount, setIncomeSourceAmount] = useState(null);
+  const [ paycheckAmount, setPaycheckAmount] = useState(null);
+  const [ rentAmount, setRentAmount ] = useState(null);
+  const [ utilitiesAmount, setUtilitiesAmount ] = useState(null);
+  const [ reoccurringBills, setreoccurringBills] = useState(null);
+  const [ gasAmount, setGasAmount ] = useState(null);
+  const [ foodAmount, setFoodAmount ] = useState(null);
 
-  const onEdit = ({id, incomeSourceAmount}) => {
+  const onEdit = ({id, currentPaycheckAmount, currentRentAmount}) => {
       setInEditMode({
           status: true,
           rowKey: id
       })
-      setIncomeSourceAmount(incomeSourceAmount);
+      setPaycheckAmount(currentPaycheckAmount);
 }
 
 //fetch go here?
+const updatePaycheck = ({id, newPaycheckAmount}) => {
+    console.log("paycheck updated")
+  // fetch(`${BILLION_DOLLAR_BUDGET_API_URL}/${id}`, {
+  //     method: "PATCH",
+  //     body: JSON.stringify({
+  //         income_source_amount: newUnitPrice
+  //     }),
+  //     headers: {
+  //         "Content-type": "application/json; charset=UTF-8"
+  //     }
+  // })
+  //     .then(response => response.json())
+  //     .then(json => {
+  //         // reset inEditMode and unit price state values
+  //         onCancel();
 
-const onSave = ({id, newIncomeSourceAmount}) => {
-  updateInventory({id, newIncomeSourceAmount});
+  //         // fetch the updated data
+  //         fetchInventory();
+  //     })
+}
+const onSave = ({id, newPaycheckAmount}) => {
+  updatePaycheck({id, newPaycheckAmount});
 }
 
 const onCancel = () => {
@@ -32,44 +57,104 @@ const onCancel = () => {
       status: false,
       rowKey: null
   })
-  // reset the unit price state value
-  setIncomeSourceAmount(null);
+  // reset the paycheckAmount state value
+  setPaycheckAmount(null);
 }
   
+
   return (
     <>
         <h3>Monthly Income</h3>
       <div>
-        <table>
+        <Table striped brodered hover variant='dark'>
             <thead>
             <tr>
-              <th>Income Source</th>
-              <th>Amount</th>
+              <th>Paycheck</th>
+              <th>Rent</th>
+              <th>Utilities</th>
+              <th>Reoccurring Bills</th>
+              <th>Gas</th>
+              <th>Food</th>
+              <th>Action</th>
             </tr> 
             </thead>
           <tbody>
           {
               data.map((user) => (
                 <tr key={user.id}>
-                  <td>{user.income_source}</td>
                   <td>
                       {
                           inEditMode.status && inEditMode.rowKey === user.id ? (
-                              <input value={incomeSourceAmount}
-                                 onChange={(event) => setIncomeSourceAmount(event.target.value)}
+                              <input value={paycheckAmount}
+                                 onChange={(event) => setPaycheckAmount(event.target.value)}
                               />
                           ) : (  
-                              user.income_source_amount
+                              user.paycheck
+                          )
+                      }
+                  </td>
+                  <td>
+                  {
+                          inEditMode.status && inEditMode.rowKey === user.id ? (
+                              <input value={rentAmount}
+                                 onChange={(event) => setRentAmount(event.target.value)}
+                              />
+                          ) : (  
+                              user.rent
+                          )
+                      }
+                  </td>
+                  <td>
+                  {
+                          inEditMode.status && inEditMode.rowKey === user.id ? (
+                              <input value={utilitiesAmount}
+                                 onChange={(event) => setUtilitiesAmount(event.target.value)}
+                              />
+                          ) : (  
+                              user.utilities
+                          )
+                      }
+                  </td>
+                  <td>
+                  {
+                          inEditMode.status && inEditMode.rowKey === user.id ? (
+                              <input value={reoccurringBills}
+                                 onChange={(event) => setreoccurringBills(event.target.value)}
+                              />
+                          ) : (  
+                              user.reoccurringBills
+                          )
+                      }
+                  </td>
+                  <td>
+                  {
+                          inEditMode.status && inEditMode.rowKey === user.id ? (
+                              <input value={gasAmount}
+                                 onChange={(event) => setGasAmount(event.target.value)}
+                              />
+                          ) : (  
+                              user.gas
+                          )
+                      }
+                  </td>
+                  <td>
+                      {
+                          inEditMode.status && inEditMode.rowKey === user.id ? (
+                              <input value={foodAmount}
+                                 onChange={(event) => setFoodAmount(event.target.value)}
+                              />
+                          ) : (  
+                              user.food
                           )
                       }
                   </td>
                   <td>
                      {
-                        inEditMode.status && inEditMode.rowKey === item.id ? (
+                        inEditMode.status && inEditMode.rowKey === user.id ? (
                           <>
                               <button                     
                                 className={"btn btn-success"}
-                                onClick={() => onSave({id: item.id, newUnitPrice: unitPrice})}
+                                onClick={() => onSave({id: user.id, newPaycheckAmount: paycheckAmount})}
                               >Save
                               </button>
 
@@ -83,7 +168,7 @@ const onCancel = () => {
                       ) : (
                               <button
                                 className={"btn btn-primary"}
-                                onClick={() => onEdit({id: item.id, currentUnitPrice: item.unit_price})}
+                                onClick={() => onEdit({id: user.id, paycheckAmount: user.paycheck_amount})}
                               >Edit</button>
                                     )
                       }
@@ -92,11 +177,11 @@ const onCancel = () => {
               ))
           }
           </tbody>
-         </table>
+         </Table>
       </div>
       <div>
           <h3>Monthly Expense (fill in amounts to add your total expenses)</h3>
-          <Table>
+          <table>
            <thead>
               <tr>
                 <th>Item</th>
@@ -125,7 +210,7 @@ const onCancel = () => {
                 <td>$400</td>
               </tr>
             </tbody>
-         </Table>
+         </table>
          <hr/>
         <input type='text' />
         <button className='btn btn-primary'>add expenses</button>
