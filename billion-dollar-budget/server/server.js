eadd3031d20d8eb0ccbc555e2f2feced024c266a
chris chubb <chubb91chris@icloud.com>
2022-07-09 03:48:30 -0400

    added schemas

--- billion-dollar-budget/server/server.js
+++ billion-dollar-budget/server/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const { ApolloSever } = require('apollo-server-express');
+const {typeDefs, resolvers} = require('/schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const server = new ApolloSever({
+    typeDefs,
+    resolvers
+})
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const startApolloServer = async (typeDefs,resolvers) => {
+    await server.start();
+    server.applyMiddleware({app});
 
 db.once('open', () => {
     app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
 });
 });
+}
+
+startApolloServer(typeDefs,resolvers);no
