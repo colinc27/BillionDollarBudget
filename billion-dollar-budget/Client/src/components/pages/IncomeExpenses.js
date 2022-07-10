9d328b7bc75f010d6ef3e4027d7dbfeba3c35078
robertarthury <robertarthury@gmail.com>
2022-07-10 12:36:46 -0400

    IE component

--- billion-dollar-budget/Client/src/components/pages/IncomeExpenses.js
+++ billion-dollar-budget/Client/src/components/pages/IncomeExpenses.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, } from 'react'
 import Table from 'react-bootstrap/Table'
 import '../styles/Home.css'
 import 'react-bootstrap'
