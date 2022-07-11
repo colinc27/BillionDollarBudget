e7f4ed09fbe415d5595cb6ed1b045bc14463f997
robertarthury <robertarthury@gmail.com>
2022-07-11 17:11:40 -0400

    added logout button, messed with some more buttons

--- billion-dollar-budget/Client/src/components/pages/Header.js
+++ billion-dollar-budget/Client/src/components/pages/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../styles/Home.css'
+import 'react-bootstrap'
 
 const Header = () => {
   return (
     <div>
         <h2>Billion Dollar Budget Tracker</h2>
-        <button>Logout</button>
+        <button className="btn btn-secondary">Logout</button>
     </div>
   
 
