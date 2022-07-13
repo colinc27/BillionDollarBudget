2ca2d8ac09a2da428f3e15355eb6ae400f9e4b6b
colinc27 <colinchristoph27@gmail.com>
2022-07-13 13:20:56 -0700

    commit

--- billion-dollar-budget/Client/src/components/pages/Chart.js
+++ billion-dollar-budget/Client/src/components/pages/Chart.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import Sidebar from "./Sidebar";
 
-
-const Chart = () => {
+const Chart = ({expectedData}) => {
 
     const expense = [
         {
--- billion-dollar-budget/Client/src/components/pages/Sidebar.js
+++ billion-dollar-budget/Client/src/components/pages/Sidebar.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import { slide as Menu } from 'react-burger-menu';
+// import { ADD_BUDGET } from './utils/mutations';
+// import { useMutation } from '@apollo/client';
+// import { QUERY_BUDGET} from './utils/queries'
 
 
 
 const Sidebar = () =>{
+    
     const [monthlyIncome, setIncome] = useState(0);
     const [expectedRent, setRent] = useState(0);
     const [expectedUtilities, setUtilities] = useState(0);
@@ -11,7 +15,20 @@ const Sidebar = () =>{
     const [expectedFood, setFood] = useState(0);
     const [expectedGas, setGas] = useState(0);
 
-      // update state based on form input changes
+    // const [updateBudget, { error }] = useMutation(ADD_BUDGET, {
+    //     update(cache, { data: { expectedData } }) {
+    //     try {
+    //       const { expected } = cache.readQuery({ query: QUERY_BUDGET });
+    //       cache.writeQuery({
+    //         query: QUERY_BUDGET,
+    //         data: { expected: { ...expected, expectedData: [...expected.expectedData, expectedData] } },
+    //       });
+    //     } catch (e) {
+    //       console.warn("First thought insertion by user!")
+    //     }
+    //   }
+    // })
+
   const handleChangeIncome = (event) => {
       setIncome(event.target.value);
 }
@@ -35,6 +52,7 @@ const handleChangeGas = (event) => {
     event.preventDefault();
     const expectedData = [monthlyIncome,expectedFood,expectedGas,expectedMisc,expectedRent,expectedUtilities]
     console.log(expectedData)
+    
   };
 
 return (
@@ -66,73 +84,7 @@ return (
 export default Sidebar;
           // submit form
 
-// class Sidebar extends React.Component{
-//     constructor(props) {
-//         super(props);
-//         this.state={
-//             monthlyIncome: 0,
-//             expectedRent: 0,
-//             expectedUtilities: 0,
-//             expectedMisc: 0,
-//             expectedFood: 0,
-//             expectedGas: 0
-//         }
 
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//         this.handleChange = this.handleChange.bind(this);
-//       }
-//       handleChange(e) {
-//         this.setState({[e.target.name]: e.target.value})
-//         console.log(e.target.value)
-//       }
-
-//       handleSubmit(event) {
-//         event.preventDefault();
-//         const {monthlyIncome, expectedRent, expectedUtilities, expectedMisc, expectedFood, expectedGas} =this.state
-//         console.log((monthlyIncome)); // reference by form input's `name` tag
-//         console.log((expectedRent));
-//         console.log((expectedUtilities)); 
-//         console.log((expectedMisc));
-//         console.log((expectedFood));
-//         console.log((expectedGas));
-//         const data = [monthlyIncome, expectedRent, expectedUtilities, expectedMisc, expectedFood, expectedGas]
-//         console.log(data)
-//         // fetch('/api/expected', {
-//         //   method: 'POST',
-//         //   body: data,
-//         // });
-//       }
-    
-// render(){
-//   return (
-//       <Menu>
-//         <div>
-//           <h5>Please enter your budget variables here</h5>
-//           <form onSubmit={this.handleSubmit}>
-//             <label htmlFor='monthlyIncome'>Monthly Income</label>
-//             <input type="number" name="monthlyIncome" id='monthlyIncome' defaultValue={this.state.monthlyIncome} onChange={this.handleChange}/>
-//             <label htmlFor='expectedRent'>Expected Rent</label>
-//             <input type="number" name="expectedRent" id='expectedRent' defaultValue={this.state.expectedRent} onChange={this.handleChange}/>
-//             <label htmlFor='expectedUtilities'>Expected Utilities</label>
-//             <input type="number" name="expectedUtilities" id='expectedUtilities' defaultValue={this.state.expectedUtilities} onChange={this.handleChange}/>
-//             <label htmlFor='expectedMisc'>Expected Misc. Expenses</label>
-//             <input type="number" name="expectedMisc" id='expectedMisc' defaultValue={this.state.expectedRent} onChange={this.handleChange}/>
-//             <label htmlFor='expectedFood'>Expected Food</label>
-//             <input type="number" name="expectedFood" id='expectedFood' defaultValue={this.state.expectedFood} onChange={this.handleChange}/>
-//             <label htmlFor='expectedGas'>Expected Gas</label>
-//             <input type="number" name="expectedGas" id='expectedGas' defaultValue={this.state.expectedGas} onChange={this.handleChange}/>
-//             <button type="submit" id="save" class="btn btn-success mx-2">Save</button>
-//           </form>
-//           {/* <div class="d-flex flex-row my-2">         
-//           <h6 class="px-2">Monthly Income</h6>
-//              <input class="form-control form-control-sm " id="budget" type="text" placeholder="$300"></input>
-//              </div> */}
-//         </div>
-//     </Menu>
-//   )
-// }
-// }
-// export default Sidebar;
 
 
 
