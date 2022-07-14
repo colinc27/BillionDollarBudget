ff04218d89d279ff1c949bac21a7f597ab522454
chris chubb <chubb91chris@icloud.com>
2022-07-14 01:08:50 -0400

    changed routes

--- billion-dollar-budget/Client/src/pages/Home.js
+++ billion-dollar-budget/Client/src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import  Header  from './components/Header/index.js';
-import Balance  from './components/Balance/index.js';
-import IncomeExpenses from './components/IncomeExpenses/index.js';
-import Chart from "./components/Chart/index.js";
-import Footer from './components/Footer/index.js';
-import './App.css';
+import  Header  from './components/Header/';
+import Balance  from './components/Balance/';
+import IncomeExpenses from './components/IncomeExpenses/';
+import Chart from "./components/Chart/";
+import Footer from './components/Footer/';
+import '../styles/Home.css';
 // import Sidebar from './components/pages/Sidebar';
 function Home() {
   return (
--- billion-dollar-budget/Client/src/pages/Login.js
+++ billion-dollar-budget/Client/src/pages/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/mutations';
+import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from '../../utils/auth';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
